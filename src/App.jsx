9f88a8452c4039f17891750972c0ed2dd548ca77
Sathvik Balu Talagama <sbtalagama@gmail.com>
2025-05-04 15:16:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-3xl font-bold text-green-800'>
+      Namaste we're building a Netflix GPT 
+    </div>
+  )
+}
+
+export default App
